042aa060e3ba468f738fa4976ee5cba879f06317
nayakvinaya <vinayanayak1996@gmail.com>
2023-04-01 13:40:22 -0400

    Delete mapreduce directory

--- mapreduce/mrjob.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -files mapper.py, reducer.y -mapper mapper.py -reducer reducer.py -input /user/cloudera/mapreduce/test assign ment.txt -output /user/cloudera/mapreduce/outputassignment
