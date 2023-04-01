83866e0a6f19a2b9d4b611326bab2c471b52baf3
nayakvinaya <vinayanayak1996@gmail.com>
2023-04-01 09:53:26 -0700

    Add files via upload

---
+++ mapreduce/mapper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    numbers = list(map(int, line.strip().split()))
+    # increase counters
+    for value in numbers:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print '%s\t%s' % (value, 1)
+
+
+        
+
---
+++ mapreduce/reducer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""reducer.py"""
+
+from operator import itemgetter
+import sys
+
+total = 0
+count = 0
+
+# input comes from STDIN
+for line in sys.stdin:
+    value, count_str = line.strip().split('\t')
+    count = count + int(count_str)
+    total = total + int(value) * int(count_str)
+print '%s' % (total/count)
+
