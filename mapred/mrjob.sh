9dad7d14966ce43930814173d1fccd387ee28707
nayakvinaya <vinayanayak1996@gmail.com>
2023-04-01 14:21:06 -0400

    Delete mapred directory

--- mapred/mrjob.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-/usr/bin/hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
-  -files mapper.py, reducer.py
-  -mapper mapper.py \
-  -reducer reducer.py \
-  -input /mapreduce/test.txt \
-  -output /mapreduce/output
